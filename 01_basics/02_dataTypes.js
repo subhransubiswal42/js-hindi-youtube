6934b93d34913a2477e038c6a61e049f17cd9c32
Subhransu Biswal <subhransubiswal423@gmail.com>
2026-02-01 16:34:13 +0000

    DataTypes Learn

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) //We are using nodejs, not browser
+
+
+console.log( 3 + 
+    3) // code readability should be high
+
+
+console.log("Subhransu")
+
+
+let name = "subhransu" //String DataType
+let age = 19 //integer DataType
+let isLogin = true //Boolean DataType
+
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
