4c595ab371ee30e03e961d19f290eba33a200650
Subhransu Biswal <subhransubiswal423@gmail.com>
2026-02-01 15:17:09 +0000

    Learn Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "subhraansu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState ;
+
+// accountId = 2
+
+accountEmail = "sinu@gmail.com"
+accountPassword = "6852568"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
